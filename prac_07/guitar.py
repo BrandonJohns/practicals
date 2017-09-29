ebd9f6069a89331fa900c3da326782c7b58c01af
BrandonJohns <brandon.johns@my.jcu.edu.au>
2017-09-29 11:15:12 +1000

    started from scratch

---
+++ prac_07/guitar.py
@@ -0,0 +1,17 @@
+CURRENT_YEAR = 2017
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self, year):
+        age = CURRENT_YEAR - year
+        return age
+
+    def is_vintage(self, year):
+        if CURRENT_YEAR - year >= 50:
+            return True
---
+++ prac_07/guitar_test.py
@@ -0,0 +1,10 @@
+from prac_07.guitar import Guitar
+
+def main():
+    guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    guitar2 = Guitar("Kmart special", 2016, 25.99)
+    age = guitar1.get_age
+    print(age)
+    print(guitar1
+    print(guitar2)
+main()
