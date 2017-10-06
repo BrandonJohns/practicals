1e732e7b0fb8c073df24be53873231148dd946e3
BrandonJohns <brandon.johns@my.jcu.edu.au>
2017-10-06 12:02:28 +1000

    finish prac 8

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -2,10 +2,11 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 
 def main():
-    hummer1 = SilverServiceTaxi("Hummer", 200, 2)
+    hummer1 = SilverServiceTaxi("Hummer", 200, 2.0)
     print(hummer1)
     hummer1.drive(18)
     print("${:.2f}".format(hummer1.get_fare()))
+    print(hummer1)
 
 
 main()
