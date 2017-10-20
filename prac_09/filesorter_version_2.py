8908590462fe171b31676da16c1fcd70378d1a90
BrandonJohns <brandon.johns@my.jcu.edu.au>
2017-10-20 11:40:55 +1000

    fsdjk

---
+++ prac_09/filesorter_version_2.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+
+def main():
+    os.chdir('FilesToSort')
+    print(os.getcwd())
+    extensions = []
+    for filename in os.listdir('.'):
+        if not os.path.isdir(filename):
+            extension = filename.split(".")
+            extension = extension[1]
+            if extension not in extensions:
+                extensions.append(extension)
+
+                #try:
+                #    os.mkdir(extension)
+                #except:
+                 #   print("directorys already created")
+            shutil.move(filename, extension)
+
+main()
